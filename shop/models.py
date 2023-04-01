fa7a48cd5c55deec2d834d9a6ed000a1264f71d0
gconner2016 <gconner@unomaha.edu>
2023-04-01 16:41:40 -0500

    Update models.py
    Added model for profile

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -45,3 +46,11 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('shop:product_detail',
                            args=[self.id, self.slug])
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile' #show how we want it to be displayed
